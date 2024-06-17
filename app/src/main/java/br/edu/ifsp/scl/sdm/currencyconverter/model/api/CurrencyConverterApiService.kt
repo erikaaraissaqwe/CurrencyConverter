9fcb7b7b74d32d2aa44e3969e452a4b2ff0fdf91
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2024-06-16 23:52:11 -0300

    Aula 01

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
@@ -0,0 +1,28 @@
+package br.edu.ifsp.scl.sdm.currencyconverter.model.api
+
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.ConversionResult
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.CurrencyList
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface CurrencyConverterApiService {
+    // https://rapidapi.com/natkapral/api/currency-converter5
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: 6acc2eb728mshf0b41abd4d4597ep1bf582jsn77e505dee1fa")
+    @GET("list")
+    fun getCurrencies(): Call<CurrencyList>
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: 6acc2eb728mshf0b41abd4d4597ep1bf582jsn77e505dee1fa")
+    @GET("convert")
+    fun convert(
+        @Query("from") from: String,
+        @Query("to") to: String,
+        @Query("amount") amount: Double
+    ): Call<ConversionResult>
+}
